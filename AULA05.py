22410e6571a64e3cba5e8375c85874f109ff804e
alinyvital <alinyvital@gmail.com>
2022-08-16 10:50:05 -0300

    mais escritas

--- AULA05.py
+++ AULA05.py
@@ -4,4 +4,19 @@ print(type(lista_aniversario))
 
 #LISTA PRECISA TER COLCHETES
 
-convidado_conf = ['André', True]
+numeros = [1,2,3,4,5,6,7,8,9]
+print(max(numeros))
+print(min(numeros))
+print(sum(numeros))
+lista = ['André', 'Aliny', 'Catherine']
+print(lista)
+lista.append('de Vital')
+print(lista)
+lista.insert(0,'Oliveira')
+print(lista)
+print(lista.pop(0))
+lista.remove('André')
+print(lista)
+print(lista.index('Aliny'))
+
+
